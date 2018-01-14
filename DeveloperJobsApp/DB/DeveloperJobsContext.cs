ed9e0041911e47a5fd664b2131905b3cb6ebd3b6
Enmanuel García <egarciag@ucenfotec.ac.cr>
2018-01-13 22:44:10 -0600

    Implemented Entity Framework Core.

---
+++ DeveloperJobsApp/DB/DeveloperJobsContext.cs
@@ -0,0 +1,27 @@
+﻿using DeveloperJobsApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeveloperJobsApp.DB
+{
+    public class DeveloperJobsContext : DbContext
+    {
+        public DbSet<DeveloperJob> DeveloperJobs { get; set; }
+
+        public DeveloperJobsContext(DbContextOptions<DeveloperJobsContext> developerJobsContextOptions) 
+            : base(developerJobsContextOptions) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- DeveloperJobsApp/Startup.cs
+++ DeveloperJobsApp/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DeveloperJobsApp.DB;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,17 +14,20 @@ namespace DeveloperJobsApp
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<DeveloperJobsContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("DeveloperJobsDatabase")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
